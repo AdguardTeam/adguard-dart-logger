e239bd467007928de523f926889cfaf8a031b0d6
Konstantin Gorynin <k.goryinin@adguard.com>
2024-11-22 15:40:38 +0300

    Refactor platform path util to use _kIsWeb constant

--- lib/src/util/platform_path_util.dart
+++ lib/src/util/platform_path_util.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 
-import 'package:adguard_logger/src/util/platform_util.dart';
-
-
 abstract class PlatformPathUtil {
-  static String get platformSeparator => PlatformUtil.kIsWeb ? '/' : Platform.pathSeparator;
+  static bool get _kIsWeb => const bool.fromEnvironment('dart.library.js_util');
+
+  static String get platformSeparator => _kIsWeb ? '/' : Platform.pathSeparator;
 }
--- lib/src/util/platform_util.dart
+++
@@ -1,3 +0,0 @@
-abstract class PlatformUtil {
-  static bool get kIsWeb => const bool.fromEnvironment('dart.library.js_util');
-}
